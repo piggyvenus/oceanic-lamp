fea4728d38e0c91aaf6c52c90c571ec86644a446
piggyvenus <piggyvenus@gmail.com>
2019-10-04 21:29:32 -0700

    test properties

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
